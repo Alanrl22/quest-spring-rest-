1f1a7bf04a2f53ed21b7137cbb3762f9ba23c7e2
Alanrl22 <alan.raoul22@gmail.com>
2020-06-23 17:24:15 +0200

    Spring Rest

---
+++ src/main/java/com/wildcodeschool/springrest/controller/BookController.java
@@ -0,0 +1,50 @@
+package com.wildcodeschool.springrest.controller;
+
+import com.wildcodeschool.springrest.entity.Book;
+import com.wildcodeschool.springrest.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class BookController {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @GetMapping("/books")
+    public List<Book> index(){
+        return bookRepository.findAll();
+    }
+    @GetMapping("/books/{id}")
+    public Book show(@PathVariable Long id){
+        return bookRepository.findById(id).get();
+    }
+
+    @PostMapping("/books/search")
+    public List<Book> search(@RequestBody Map<String, String> body){
+        String searchTerm = body.get("text");
+        return bookRepository.findByTitleContainingOrDescriptionContaining(searchTerm, searchTerm);
+    }
+
+    @PostMapping("/books")
+    public Book create(@RequestBody Book book){
+        return bookRepository.save(book);
+    }
+
+    @PutMapping("/books/{id}")
+    public Book update(@PathVariable Long id, @RequestBody Book book){
+        Book bookToUpdate = bookRepository.findById(id).get();
+        bookToUpdate.setTitle(book.getTitle());
+        bookToUpdate.setDescription(book.getDescription());
+        return bookRepository.save(bookToUpdate);
+    }
+
+    @DeleteMapping("books/{id}")
+    public boolean delete(@PathVariable Long id){
+        bookRepository.deleteById(id);
+        return true;
+    }
+}
